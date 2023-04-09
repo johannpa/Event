f24caee645bd3a18ca9d70bfd62884647ac93825
johannpa <johann_paque@hotmail.fr>
2023-04-10 00:02:13 +0200

    to stop the bubbling

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const divs = document.querySelectorAll('div');
 
 function logText(e){
     console.log(this.classList.value);
+    e.stopPropagation(); // stop bubbling!
+    // console.log(this);
 }
 
 divs.forEach(div => div.addEventListener('click', logText, {
-    capture: true
+    capture: true // this will make the event bubble up
 }));
