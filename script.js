9410f6053bf84ac2d8381d205bef236c3a0fecff
johannpa <johann_paque@hotmail.fr>
2023-04-10 00:08:09 +0200

    You can click the button only one time

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const divs = document.querySelectorAll('div');
+const button = document.querySelector('button');
 
 function logText(e){
     console.log(this.classList.value);
-    e.stopPropagation(); // stop bubbling!
+    // e.stopPropagation(); // stop bubbling!
     // console.log(this);
 }
 
 divs.forEach(div => div.addEventListener('click', logText, {
-    capture: true // this will make the event bubble up
+    capture: true, // this will make the event bubble up
+    once : true // this will make the event fire only once
 }));
+
+button.addEventListener('click', () => {
+    console.log('Click!!!');
+}, {
+    once : true // this will make the event fire only once
+});
